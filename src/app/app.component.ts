4feb523836fca48ee2e8968ab685b8dd5a68838d
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-09 16:26:37 +0900

    InjectionToken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken, Inject } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+
+export const BACKEND_URL = new InjectionToken('BackendUrl');
 
 @Component({
   selector: 'ng-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: BACKEND_URL,
+      useValue: 'http://myQAserver.com'
+    }
+  ]
 })
 export class AppComponent {
   isNavigating = false;
-  constructor(private router: Router) {
+  constructor(@Inject(BACKEND_URL) public url, private router: Router) {
     this.router.events.subscribe(
       (event) => {
         if (event instanceof NavigationStart) {
